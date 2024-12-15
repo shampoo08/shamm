46f8ae4c7d3158c3f569c9ddd10696d74b2c1e9c
shampoo08 <36780@bpk.ac.th>
2024-12-15 21:52:10 +0700

    Add files via upload

---
+++ หาพื้นที่สามเหลี่ยม.py
@@ -0,0 +1,10 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("โปรเเกรมคำนวณพื้นที่สามเหลี่ยม")
+... A = 0.25
+... B = 5
+... print("กำหนดให้ A = 0.25 เป็นค่าฐาน เเละ B = 5 เป็นค่าความสูง")
+... print("สูตรหาพื้นที่สามเหลี่ยม ( ฐาน x สูง x 1/2 )")
+... print("จะได้ว่า A*B*1/2")
+... Output ="{:.6f}".format(A*B*1/2)
+... print("ค่าพื้นที่ของพื้นที่สามเหลี่ยมคือ", Output)
---
+++ เซต.py
@@ -0,0 +1,10 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("โปรเเกรมคำนวนหา Set\na={10,20,30,40,50,60}\nb={30,40,50,60,70,80}")
+... a={10,20,30,40,50,60}
+... b={30,40,50,60,70,80}
+... print("ให้นักเรียนเขียนคำสั่งด้วยภาษาไพทอนหาเซตของที่กำหนดให้เเสดงผลตามที่กำหนด")
+... print(a.union(b))
+... print(a.intersection(b))
+... print(a.difference(b))
+... print(a.symmetric_difference(b))
